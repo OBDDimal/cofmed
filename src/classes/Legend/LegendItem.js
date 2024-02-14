69507d5dd01156fa24bfaac6375386feee2f9613
Lukas Ostheimer <131896156+LukasOsti@users.noreply.github.com>
2024-02-14 16:18:09 +0100

    fix: pngs are not available in other views

--- src/classes/Legend/LegendItem.js
+++ src/classes/Legend/LegendItem.js
@@ -1,11 +1,11 @@
 export class LegendItem{
     /**
-     * 
+     *
      * @param {*} description: of the Legend Item which is shown inside the legend
      * @param {*} image: path to image of the LegenItem
      */
     constructor( description, image ){
         this.description = description;
-        this.image= image;
+        this.image = window.location.protocol + "//" + window.location.host + "/" + image;
     }
 }
